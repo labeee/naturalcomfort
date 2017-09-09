e54377abe4ff8500464b84f633079c30812a3466
RaynerMauricio <machyner@hotmail.com>
2017-09-09 04:46:58 -0300

    console
    btn main + windowOUTLOG

--- Natural Comfort_files/core.engine.30.08.17.js
+++ Natural Comfort_files/core.engine.30.08.17.js
@@ -340,7 +340,7 @@ $(document).ready(function() {
 		}
 		
 		
-		geralesquerda="<div id='windowOUTLOG' class='modal modal-wide fade'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal' aria-hidden='true'>×</button><h4 class='modal-title'>SAÍDA</h4></div><div class='modal-body'><ul id='outlog' class='list-inline'>";
+		geralesquerda="<div id='windowOUTLOG' class='modal modal-wide fade'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal' aria-hidden='true'>×</button><h4 class='modal-title'>CONSOLE</h4></div><div class='modal-body'><ul id='outlog' class='list-inline'>";
 		
 		geraldireita="</ul></div><div class='modal-footer'><button type='button' class='btn btn-default' data-dismiss='modal'>Fechar</button></div></div></div></div>";
 		
